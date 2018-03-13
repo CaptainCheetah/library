0dcc0cab48ae8409a3ecfbca281046dbc743a94a
Adrian Bartel <30985844+CaptainCheetah@users.noreply.github.com>
2018-03-13 09:06:39 -0400

    Update application.js

--- js/application.js
+++ js/application.js
@@ -13,7 +13,7 @@ LIBRARY.getLibrary = function(){
       type: 'GET',
       url: `https://${LIBRARY.cloudantconfig.account}.cloudant.com/_session`,
       crossDomain: true,
-      xhrFields.withCredentials: true,
+      xhrFields: { withCredentials: true },
       complete: (jqXHR) => {
         if (jqXHR.status === 401){
 	  $('#signin').modal('show');
