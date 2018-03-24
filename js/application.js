461364569f1b50992ecb7c5dc75eb9689f9d7733
Adrian Bartel <30985844+CaptainCheetah@users.noreply.github.com>
2018-03-24 19:13:49 -0400

    Update application.js

--- js/application.js
+++ js/application.js
@@ -133,8 +133,8 @@ if (LIBRARY.getCredentials()){
 		  // {"data":"doc._id"},
 		  {"data":"doc.title","title":"Title"},
 		  {"data":"doc.author","title":"Author"},
-		  {"title":"Status", "defaultContent", "status"},
-		  {"title":"Actions", "defaultContent", "actions"},
+		  {"title":"Status", "defaultContent": "status"},
+		  {"title":"Actions", "defaultContent": "actions"},
 		]
   });
 }
