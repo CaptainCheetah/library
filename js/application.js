0b72775f88b3871e85ad51c5b9e708dcd445433f
Adrian Bartel <30985844+CaptainCheetah@users.noreply.github.com>
2018-03-25 14:40:54 -0400

    Update application.js

--- js/application.js
+++ js/application.js
@@ -144,6 +144,16 @@ LIBRARY.getCredentials = function(){
   return false;
 }
 
+LIBRARY.urlParam = function(name){
+    var results = new RegExp('[\?&]' + name + '=([^]*)').exec(window.location.href);
+    if (results==null){
+       return null;
+    }
+    else{
+       return results[1] || 0;
+    }
+}
+
 LIBRARY.init = function(){
   $('#credentialsButton').click(function(){
     LIBRARY.saveCredentials();
